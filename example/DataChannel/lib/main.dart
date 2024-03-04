3dd498b4231a442d81524b11a5db72636cfb965c
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2024-03-05 00:49:46 +0300

    Make samples compatible with flutter web

--- example/DataChannel/lib/main.dart
+++ example/DataChannel/lib/main.dart
@@ -2,12 +2,14 @@
 
 import 'dart:core';
 import 'dart:io';
+
 import 'package:ant_media_flutter/ant_media_flutter.dart';
-import 'package:flutter/material.dart';
 import 'package:datachannel/datachannel.dart';
 import 'package:datachannel/route_item.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(const MaterialApp(
       home: MyApp(),
@@ -40,7 +42,7 @@ class _MyAppState extends State<MyApp> {
     _initItems();
     AntMediaFlutter.requestPermissions();
 
-    if (Platform.isAndroid) {
+    if (!kIsWeb && Platform.isAndroid) {
       AntMediaFlutter.startForegroundService();
     }
   }
