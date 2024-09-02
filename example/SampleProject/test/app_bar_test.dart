c4dc65656c523dda035d0632e41c4d79442e05ab
filip-ams <filip.veselinovic@antmedia.io>
2024-09-02 11:02:19 +0200

    m

--- example/SampleProject/test/app_bar_test.dart
+++ example/SampleProject/test/app_bar_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter_test/flutter_test.dart';
 import 'package:example/main.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('AppBar title is displayed', (WidgetTester tester) async {
