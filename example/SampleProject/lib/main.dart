8728879760134bd22d414e884c04a15117ddf8ca
filip-ams <filip.veselinovic@antmedia.io>
2024-09-23 10:25:08 +0200

    minor change

--- example/SampleProject/lib/main.dart
+++ example/SampleProject/lib/main.dart
@@ -67,7 +67,6 @@ class _MyAppState extends State<MyApp> {
               onPressed: () {
                 _showServerAddressDialog(context);
               },
-
               tooltip: 'setup',
             ),
           ],
