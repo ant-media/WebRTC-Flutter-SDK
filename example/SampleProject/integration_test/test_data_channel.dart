e98a5b72d3a9115c5a047d461850701c9ac9f51e
filip-ams <filip.veselinovic@antmedia.io>
2024-10-28 11:05:55 +0100

    Tests

--- example/SampleProject/integration_test/test_data_channel.dart
+++ example/SampleProject/integration_test/test_data_channel.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:example/main.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
   // Initialize the integration test bindings.
