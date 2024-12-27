64ad2129b8c30faec90f4a809a5f42f98dbc70f8
filip-ams <filip.veselinovic@antmedia.io>
2024-12-27 09:26:34 +0100

    push_notifications

--- example/SampleProject/windows/flutter/generated_plugin_registrant.cc
+++ example/SampleProject/windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <ant_media_flutter/ant_media_flutter.h>
+#include <firebase_core/firebase_core_plugin_c_api.h>
 #include <flutter_webrtc/flutter_web_r_t_c_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  AntMediaFlutterRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("AntMediaFlutter"));
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
   FlutterWebRTCPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterWebRTCPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
--- example/SampleProject/windows/flutter/generated_plugins.cmake
+++ example/SampleProject/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  ant_media_flutter
+  firebase_core
   flutter_webrtc
   permission_handler_windows
 )
