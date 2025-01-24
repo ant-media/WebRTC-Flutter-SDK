58bf1d64df615cad1aa58c44133f1804d5f40d29
filip-ams <filip.veselinovic@antmedia.io>
2025-01-24 13:09:03 +0100

    push_notifications

--- lib/firebase_api.dart
+++ lib/firebase_api.dart
@@ -1,6 +1,23 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-// You can configure the firebase push notifications with flutterfire (https://firebase.flutter.dev/docs/overview)
+// You can configure the firebase push notifications with flutter-fire (https://firebase.flutter.dev/docs/overview) after which you can use it in the example app like this:
+
+//import 'package:ant_media_flutter/firebase_api.dart';
+
+// void main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   await FirebaseApi().initNotifications();
+//   return runApp(
+//       const MaterialApp(
+//         home: MyApp(),
+//         debugShowCheckedModeBanner: false,
+//       )
+//   );
+// }
+
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print("Title: ${message.notification?.title}");
